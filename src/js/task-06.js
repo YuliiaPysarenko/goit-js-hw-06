6e8cb795ce6bc2b7c6a7f66c7f16c94dae5a1659
Yuliia Pysarenko <yuliapysarenko@gmail.com>
2021-11-01 00:09:35 +0200

    поправляє показ класу в задачі 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,9 +1,15 @@
 const input = document.querySelector("#validation-input");
 
-input.addEventListener("blur", () => {
-    if (input.value.length <= input.dataset.length) {
+input.addEventListener("blur", (event) => {
+    let inputValue = event.currentTarget.value;
+
+    if (inputValue.length === Number(input.dataset.length)) {
         input.classList.add("valid");
     } else {
         input.classList.add("invalid");
     }
+});
+
+input.addEventListener("focus", () => {
+    input.classList.remove("valid") || input.classList.remove("invalid");
 });
